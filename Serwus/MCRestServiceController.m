6c569da8a0d4160442593292a10a3a14e15d904e
Madis Nõmme <madis.nomme@gmail.com>
2011-07-06 15:45:12 +0300

    Added README

--- Serwus/MCRestServiceController.m
+++ Serwus/MCRestServiceController.m
@@ -7,11 +7,45 @@
 //
 
 #import "MCRestServiceController.h"
-
+#import "MCRestResourceProvider.h"
 
 @implementation MCRestServiceController
 
 @synthesize restResource;
+
+#pragma mark UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+	NSInteger rows = 0;
+	return rows;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+	NSInteger sections = 0;
+	return sections;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *cellIdentifier = @"<#MyCell#>";
+	
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if (cell == nil) {
+        cell = [[[UITableViewCell alloc] initWithFrame:CGRectZero reuseIdentifier:cellIdentifier] autorelease];
+    }
+	
+    <#customize cell before it is shown#>
+	
+    return cell;
+}
+
+
+#pragma mark UITableViewDelegate
+// All method optional
+// - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath;
+// - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section;
+// - (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section;
+// - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath;
 
 
 #pragma mark NSObject
